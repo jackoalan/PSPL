8f5de815f8c7d9bdf423f824b05d9743c946ee95
Jack Andersen <jackoalan@gmail.com>
2013-04-16 16:59:50 -0500

    More work on GX and started CMake

--- Extensions/GXSupport/gx_runtime.c
+++ Extensions/GXSupport/gx_runtime.c
@@ -9,4 +9,5 @@
 
 #include "gx_runtime.h"
 
-
+/* Bytecode playback routine (implemented in separate assembly file) */
+extern void pspl_gx_playback(void* wgPipe, void* stream_start, size_t stream_len);
--- Extensions/GXSupport/gx_runtime.h
+++ Extensions/GXSupport/gx_runtime.h
@@ -9,14 +9,6 @@
 #ifndef PSPL_gx_runtime_h
 #define PSPL_gx_runtime_h
 
-/* Which symbol holds the address to our write-gather pipe? */
-#ifndef GX_WGPIPE_SYM
-#define GX_WGPIPE_SYM wgPipe
-#endif
-
-
-#include <ogc/gx.h>
-
 
 
 
