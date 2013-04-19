75bfbae63de160d0d0fe22041f747421bf099c78
Jack Andersen <jackoalan@gmail.com>
2013-04-19 10:10:29 -0500

    Initial CMake files written

---
+++ cmake/PSPLExtensionMacros.cmake
@@ -0,0 +1,15 @@
+cmake_minimum_required(VERSION 2.8)
+
+
+# Augment a PSPL extension with an archivable object class
+macro(pspl_add_extension_class extension_name class_id)
+  
+endmacro()
+
+
+# Augment a PSPL extension with a toolchain extension
+macro(pspl_add_toolchain_extension extension_name )
+  
+endmacro(pspl_add_toolchain_extension)
+
+
--- include/PSPL/PSPLExtension.h
+++ include/PSPL/PSPLExtension.h
@@ -11,19 +11,55 @@
 
 
 #include <PSPL/PSPL.h>
+#include <PSPL/Toolchain/PSPLToolchainExtension.h>
+#include <PSPL/Runtime/PSPLRuntimeExtension.h>
+
+
+/* Forward decl of class struct type */
+typedef struct _pspl_class pspl_class_t;
+
+
+/* Common extension definition structure
+ * 
+ * This structure provides a means to compose object classes,
+ * toolchain extensions and runtime extensions together into a
+ * common namespace. This namespace persists between the offline
+ * toolchain and within the runtime later.
+ */
+typedef struct _pspl_extension {
+    // Unique name of extension
+    const char* extension_name;
+    
+    // NULL-terminated array of composed `pspl_class_t` object classes
+    const pspl_class_t class_array[];
+    
+    // NULL-terminated array of composed `pspl_toolchain_extension_t`
+    // (NULL if accessed from runtime)
+    const pspl_toolchain_extension_t toolchain_extension_array[];
+    
+    // NULL-terminated array of composed `pspl_runtime_extension_t`
+    // (NULL if accessed from toolchain)
+    const pspl_runtime_extension_t runtime_extension_array[];
+    
+} pspl_extension_t;
 
 
 /* Common object class structure 
  *
- * This structure is initialised by any extensions wishing to add*/
-typedef struct _pspl_object_class {
-    // 4-byte type
-    uint8_t type[4];
+ * This structure is initialised by any extensions wishing to define 
+ * an archivable C-structure storable within a PSPL package.
+ */
+struct _pspl_class {
+    // 4-byte class identifier (present in class instances for association)
+    uint8_t class_id[4];
+    
+    // Pointer to extension defining class
+    _pspl_extension_t* class_extension;
     
     // Size of the entire object (sum of all inheriting structure sizes)
-    DECL_BI_U32(size);
+    uint32_t size;
     
-} pspl_object_t;
+};
 
 
 #endif
