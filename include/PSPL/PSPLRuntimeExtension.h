1215bad408367aeb66cbb39e5260cb45c1abffd1
Jack Andersen <jackoalan@gmail.com>
2013-05-31 12:11:41 -0400

    Added some docs

--- include/PSPL/PSPLRuntimeExtension.h
+++ include/PSPL/PSPLRuntimeExtension.h
@@ -13,14 +13,26 @@
 #include <PSPLExtension.h>
 #include <PSPLRuntime.h>
 
+/**
+ * @file PSPL/PSPLRuntimeExtension.h
+ * @brief Runtime extension API
+ * @ingroup PSPL
+ * @{
+ */
+
 #pragma mark Hook Types
 
-/* Embedded data object type */
+/** 
+ * Embedded data object type 
+ */
 typedef struct {
     size_t object_len;
     const void* object_data;
 } pspl_data_object_t;
 
+/**
+ * Runtime extension/platform hooks
+ */
 typedef void(*pspl_runtime_shutdown_hook)();
 typedef void(*pspl_runtime_load_object_hook)(const pspl_runtime_psplc_t* object);
 typedef void(*pspl_runtime_unload_object_hook)(const pspl_runtime_psplc_t* object);
@@ -31,18 +43,51 @@ typedef void(*pspl_runtime_bind_object_hook)(const pspl_runtime_psplc_t* object)
 
 /* These *must* be called within the `load` hook of platform or extension */
 
-/* Get embedded data object for extension by key (to hash) */
+/** 
+ * Get embedded data object for extension by key (used in hash lookup)
+ *
+ * * **This routine may only be called within extension/platform `load` hook**
+ * * This routine will only lookup objects in *hashed namespace*
+ * 
+ * @param object Read-only handle identifying embedded PSPLC object
+ *        to get data object from
+ * @param key Key string to hash and use for data object lookup
+ * @param data_object_out Pointer to app-allocated data object structure
+ *        needing to be populated (composing data ptr and length)
+ */
 int pspl_get_embedded_data_object_from_key(const pspl_runtime_psplc_t* object,
                                            const char* key,
                                            pspl_data_object_t* data_object_out);
 
-/* Get embedded object for extension by hash */
+/** 
+ * Get embedded object for extension by hash
+ *
+ * * **This routine may only be called within extension/platform `load` hook**
+ * * This routine will only lookup objects in *hashed namespace*
+ *
+ * @param object Read-only handle identifying embedded PSPLC object
+ *        to get data object from
+ * @param hash Hash to use for data object lookup
+ * @param data_object_out Pointer to app-allocated data object structure
+ *        needing to be populated (composing data ptr and length)
+ */
 int pspl_get_embedded_data_object_from_hash(const pspl_runtime_psplc_t* object,
                                             const pspl_hash* hash,
                                             pspl_data_object_t* data_object_out);
 
-/* Get embedded object for extension by index */
-int pspl_get_embedded_data_object_from_index(const pspl_runtime_psplc_t* object,
+/** 
+ * Get embedded data object for extension by index (used in integer lookup)
+ *
+ * * **This routine may only be called within extension/platform `load` hook**
+ * * This routine will only lookup objects in *integer namespace*
+ *
+ * @param object Read-only handle identifying embedded PSPLC object
+ *        to get data object from
+ * @param index Integer to use for data object lookup
+ * @param data_object_out Pointer to app-allocated data object structure
+ *        needing to be populated (composing data ptr and length)
+ */
+ int pspl_get_embedded_data_object_from_index(const pspl_runtime_psplc_t* object,
                                              uint32_t index,
                                              pspl_data_object_t* data_object_out);
 
@@ -101,6 +146,8 @@ typedef struct _pspl_runtime_platform {
     pspl_runtime_bind_object_hook bind_object_hook;
     
 } pspl_runtime_platform_t;
+
+/** @} */
 
 #endif // PSPL_RUNTIME
 #endif
