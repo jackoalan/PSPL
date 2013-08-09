b6666c8daa8f504192c6f665bf18d48e077075a0
Jack Andersen <jackoalan@gmail.com>
2013-08-09 17:53:45 -0500

    Added licence to GX RGBA encoder

--- Extensions/TextureManager/Encoders/RGB/rgba_enc.c
+++ Extensions/TextureManager/Encoders/RGB/rgba_enc.c
@@ -30,7 +30,28 @@ int RGBA_encode(pspl_malloc_context_t* mem_ctx, const uint8_t* image_in, unsigne
 typedef uint32_t u32;
 
 /* Swapping loops below are derived libogc's zlib-licenced `gxtexconv`
- * http://sourceforge.net/p/devkitpro/libogc/ci/master/tree/libogc_license.txt */
+ *
+ *   Copyright (C) 2004 - 2009
+ *    Michael Wiedenbauer (shagkur)
+ *    Dave Murphy (WinterMute)
+ *
+ *  This software is provided 'as-is', without any express or implied
+ *  warranty.  In no event will the authors be held liable for any
+ *  damages arising from the use of this software.
+ *
+ *  Permission is granted to anyone to use this software for any
+ *  purpose, including commercial applications, and to alter it and
+ *  redistribute it freely, subject to the following restrictions:
+ *
+ *  1. The origin of this software must not be misrepresented; you
+ *     must not claim that you wrote the original software. If you use
+ *     this software in a product, an acknowledgment in the product
+ *     documentation would be appreciated but is not required.
+ *  2. Altered source versions must be plainly marked as such, and
+ *     must not be misrepresented as being the original software.
+ *  3. This notice may not be removed or altered from any source
+ *     distribution.
+ */
 
 /* GX requires that texels be arranged into localised tiles (not scanlines) */
 int RGBAGX_encode(pspl_malloc_context_t* mem_ctx, const uint8_t* image_in, unsigned chan_count,
